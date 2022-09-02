e5d3ff55a9ccd8f93f724bb4911ed20d6ce3ca14
EthanKulangara <96702888+EthanKulangara@users.noreply.github.com>
2022-09-01 20:45:18 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Paste your firebaseConfig here
+const firebaseConfig = { 
+    apiKey: "AIzaSyCcNJSJEabBrL-Za4gCXtN9WH4-i7RN1Tc", 
+    authDomain: "bike-e9856.firebaseapp.com", 
+    projectId: "bike-e9856", 
+    storageBucket: "bike-e9856.appspot.com", 
+    messagingSenderId: "1024083374654", 
+    appId: "1:1024083374654:web:c3f801ec7e02ede517176f" 
+};
+//
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
